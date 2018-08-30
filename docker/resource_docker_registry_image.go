01de74a8c98eda612b082d4894ef62a257ded4b6
Marco <marco.helmich@live.com>
2018-08-30 09:34:24 -0700

    - adapted registry read logic to resource

--- docker/resource_docker_registry_image.go
+++ docker/resource_docker_registry_image.go
@@ -1,6 +1,10 @@
 package docker
 
 import (
+	"fmt"
+	"log"
+	"strings"
+
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -42,14 +46,60 @@ func resourceDockerRegistryImageCreate(d *schema.ResourceData, meta interface{})
 		return err
 	}
 
-	digest := data.DockerImages[targetName+":latest"].ID
+	if strings.Index(targetName, ":") == -1 {
+		targetName = targetName + ":latest"
+	}
+
+	digest := data.DockerImages[targetName].ID
 	d.SetId(digest)
 	d.Set("sha256_digest", digest)
 	return nil
 }
 
 func resourceDockerRegistryImageRead(d *schema.ResourceData, meta interface{}) error {
-	return dataSourceDockerRegistryImageRead(d, meta)
+	pullOpts := parseImageOptions(d.Get("target_name").(string))
+	authConfig := meta.(*ProviderConfig).AuthConfigs
+
+	// Use the official Docker Hub if a registry isn't specified
+	if pullOpts.Registry == "" {
+		pullOpts.Registry = "registry.hub.docker.com"
+	} else {
+		// Otherwise, filter the registry name out of the repo name
+		pullOpts.Repository = strings.Replace(pullOpts.Repository, pullOpts.Registry+"/", "", 1)
+	}
+
+	if pullOpts.Registry == "registry.hub.docker.com" {
+		// Docker prefixes 'library' to official images in the path; 'consul' becomes 'library/consul'
+		if !strings.Contains(pullOpts.Repository, "/") {
+			pullOpts.Repository = "library/" + pullOpts.Repository
+		}
+	}
+
+	if pullOpts.Tag == "" {
+		pullOpts.Tag = "latest"
+	}
+
+	var username string
+	var password string
+
+	if auth, ok := authConfig.Configs[normalizeRegistryAddress(pullOpts.Registry)]; ok {
+		username = auth.Username
+		password = auth.Password
+	}
+
+	digest, err := getImageDigest(pullOpts.Registry, pullOpts.Repository, pullOpts.Tag, username, password, false)
+	if err != nil {
+		digest, err = getImageDigest(pullOpts.Registry, pullOpts.Repository, pullOpts.Tag, username, password, true)
+		if err != nil {
+			return fmt.Errorf("Got error when attempting to fetch image version from registry: %s", err)
+		}
+	}
+
+	log.Printf("[DEBUG] found image %v : digest %s", d.Get("target_name").(string), digest)
+
+	d.SetId(digest)
+	d.Set("sha256_digest", digest)
+	return nil
 }
 
 func resourceDockerRegistryImageUpdate(d *schema.ResourceData, meta interface{}) error {
